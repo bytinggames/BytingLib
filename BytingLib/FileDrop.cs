ff215d1825fb73196c1457f10378adf0136eb926
bytinggames <bytinggames@gmail.com>
2022-06-02 14:34:07 +0200

    FileDrop + FileSession

---
+++ BytingLib/FileDrop.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace BytingLib
+{
+    public class FileDrop : IDisposable
+    {
+        private readonly GameWindow window;
+        private readonly Action<string> droppedFile;
+
+        public FileDrop(GameWindow window, Action<string> droppedFile)
+        {
+            this.window = window;
+            this.droppedFile = droppedFile;
+            window.FileDrop += Window_FileDrop;
+        }
+
+        public void Dispose()
+        {
+            window.FileDrop -= Window_FileDrop;
+        }
+
+        private void Window_FileDrop(object? sender, FileDropEventArgs e)
+        {
+            if (e.Files?.Length > 0)
+                droppedFile.Invoke(e.Files[0]);
+        }
+    }
+}
---
+++ BytingLib/FileSession.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BytingLib
+{
+    public class FileSession : IDisposable
+    {
+        private readonly string sessionFile;
+        private readonly string unsavedLevelFile;
+        private readonly Action<string> save;
+        private readonly Action<string> open;
+        private readonly Action newFile;
+        private readonly FileSessionData data;
+
+        public event Action<string?>? OnCurrentFileChanged;
+        public event Action<bool>? OnIsDirtyChanged;
+
+        public FileSession(string sessionFile, string unsavedLevelFile, Action<string> save, Action<string> open, Action newFile)
+        {
+            this.sessionFile = sessionFile;
+            this.unsavedLevelFile = unsavedLevelFile;
+            this.save = save;
+            this.open = open;
+            this.newFile = newFile;
+            Directory.CreateDirectory(Path.GetDirectoryName(sessionFile)!);
+            Directory.CreateDirectory(Path.GetDirectoryName(unsavedLevelFile)!);
+
+            if (File.Exists(sessionFile))
+            {
+                string json = File.ReadAllText(sessionFile);
+                var d = System.Text.Json.JsonSerializer.Deserialize<FileSessionData>(json);
+                if (d != null)
+                    data = d;
+                else
+                    throw new BytingException("couldn't deserialize json file " + sessionFile);
+            }
+            else
+            {
+                data = new FileSessionData();
+            }
+
+            data.OnCurrentFileChanged += f => OnCurrentFileChanged?.Invoke(f);
+            data.OnIsDirtyChanged += f => OnIsDirtyChanged?.Invoke(f);
+
+            // load last session file
+            if (data.IsDirty && File.Exists(unsavedLevelFile))
+            {
+                open(unsavedLevelFile);
+            }
+            else if (data.CurrentFile != null)
+            {
+                string file = data.CurrentFile;
+                Discard(false);
+                Open(file);
+            }
+        }
+
+        public void SetUnsaved() => data.IsDirty = true;
+
+        public bool IsUnsaved => data.IsDirty;
+
+        public bool Save()
+        {
+            if (data.CurrentFile == null)
+                return false;
+
+            SaveAs(data.CurrentFile);
+            return true;
+        }
+
+        public void SaveAs(string file)
+        {
+            data.CurrentFile = file;
+
+            save(file);
+            data.IsDirty = false;
+            SaveFileSession();
+        }
+
+        public bool Open(string file)
+        {
+            if (data.IsDirty)
+                return false;
+            open(file);
+            data.CurrentFile = file;
+            data.IsDirty = false;
+            SaveFileSession();
+            return true;
+        }
+
+        public string? GetCurrentFile() => data.CurrentFile;
+
+        public void Discard(bool saveSession = true)
+        {
+            data.CurrentFile = null;
+            data.IsDirty = false;
+            SaveFileSession();
+        }
+
+        public bool New()
+        {
+            if (data.IsDirty)
+                return false;
+
+            newFile();
+            data.CurrentFile = null;
+            SaveFileSession();
+            return true;
+        }
+
+        public void Dispose()
+        {
+            if (data.IsDirty)
+                save(unsavedLevelFile);
+
+            SaveFileSession();
+        }
+
+        private void SaveFileSession()
+        {
+            string json = System.Text.Json.JsonSerializer.Serialize(data);
+            using (var stream = File.CreateText(sessionFile))
+                stream.Write(json);
+        }
+
+        class FileSessionData
+        {
+            private string? _currentFile;
+            private bool _isDirty;
+
+            public string? CurrentFile
+            {
+                get => _currentFile;
+                set
+                {
+                    if (_currentFile != value)
+                    {
+                        _currentFile = value;
+                        OnCurrentFileChanged?.Invoke(_currentFile);
+                    }
+                }
+            }
+            public bool IsDirty
+            {
+                get => _isDirty;
+                set
+                {
+                    if (_isDirty != value)
+                    {
+                        _isDirty = value;
+                        OnIsDirtyChanged?.Invoke(_isDirty);
+                    }
+                }
+            }
+
+            public event Action<string?>? OnCurrentFileChanged;
+            public event Action<bool>? OnIsDirtyChanged;
+        }
+    }
+}
