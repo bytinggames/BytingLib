6c946c38966e71ea31e684336fdf67191f783950
bytinggames <bytinggames@gmail.com>
2023-01-29 11:11:25 +0100

    +

--- BytingLib/StaticUtilities/Extensions/MatrixExtension.cs
+++ BytingLib/StaticUtilities/Extensions/MatrixExtension.cs
@@ -43,10 +43,28 @@
             return Matrix.CreateFromAxisAngle(axis, angle);
         }
 
-        public static Matrix GetInverseTransposeWithoutTranslation(this Matrix m)
+        public static Matrix GetInverseTranspose3x3(this Matrix m)
         {
-            m.Translation = Vector3.Zero;
-            return Matrix.Transpose(Matrix.Invert(m));
+            m = GetInverse3x3(ref m);
+            return Matrix.Transpose(m);
+        }
+
+        public static Matrix GetInverse3x3(ref Matrix m)
+        {
+            float a00 = m[0,0], a01 = m[0,1], a02 = m[0,2];
+            float a10 = m[1,0], a11 = m[1,1], a12 = m[1,2];
+            float a20 = m[2,0], a21 = m[2,1], a22 = m[2,2];
+
+            float b01 = a22 * a11 - a12 * a21;
+            float b11 = -a22 * a10 + a12 * a20;
+            float b21 = a21 * a10 - a11 * a20;
+
+            float det = a00 * b01 + a01 * b11 + a02 * b21;
+
+            return new Matrix(b01 / det, (-a22 * a01 + a02 * a21) / det, (a12 * a01 - a02 * a11) / det, 0,
+                b11 / det, (a22 * a00 - a02 * a20) / det, (-a12 * a00 + a02 * a10) / det, 0,
+                b21 / det, (-a21 * a00 + a01 * a20) / det, (a11 * a00 - a01 * a10) / det, 0,
+                0, 0, 0, 1) ;
         }
     }
 }
