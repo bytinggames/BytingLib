5d7107c1340a5b05f131279fd403bdd28d82579e
Byting Games <bytinggames@gmail.com>
2023-03-02 11:04:34 +0100

    RectangleF begin

---
+++ BytingLib/DataTypes/RectangleF.cs
@@ -0,0 +1,26 @@
+﻿namespace BytingLib.DataTypes
+{
+    /// <summary>
+    /// not a mature class, but can be extended to be used as a Rect alternative that is a ValueType
+    /// </summary>
+    public struct RectangleF
+    {
+        public float X, Y, Width, Height;
+
+        public RectangleF(float x, float y, float width, float height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+
+        public RectangleF(Vector2 pos, Vector2 size)
+        {
+            X = pos.X;
+            Y = pos.Y;
+            Width = size.X;
+            Height = size.Y;
+        }
+    }
+}
