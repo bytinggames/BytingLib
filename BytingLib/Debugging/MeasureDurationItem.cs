6ee3162fc9a795d8730c96a0bcc81051f8d3212a
Byting Games <bytinggames@gmail.com>
2023-03-10 19:11:55 +0100

    cpu duration measurement tool

---
+++ BytingLib/Debugging/MeasureDurationItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace BytingLib
+{
+    public class MeasureDurationItem
+    {
+        const int maxSamples = 60;
+
+        List<long> samples = new();
+        long totalTicks;
+        Stopwatch sw = new();
+
+        public string StackStr { get; }
+
+        public MeasureDurationItem(int stack)
+        {
+            StackStr = "";
+            for (int i = 0; i < stack; i++)
+            {
+                StackStr += "  ";
+            }
+        }
+
+        internal void MeasureBegin()
+        {
+            sw.Restart();
+        }
+
+        internal void MeasureEnd()
+        {
+            sw.Stop();
+            long ms = sw.ElapsedTicks;
+            samples.Add(ms);
+            totalTicks += ms;
+            if (samples.Count > maxSamples)
+            {
+                totalTicks -= samples[0];
+                samples.RemoveAt(0);
+            }
+        }
+
+        public float GetAverageMS()
+        {
+            if (samples.Count == 0)
+                return -1;
+
+            return (float)totalTicks / TimeSpan.TicksPerMillisecond / samples.Count;
+        }
+    }
+}
---
+++ BytingLib/Debugging/MeasureDurations.cs
@@ -0,0 +1,44 @@
+﻿
+namespace BytingLib
+{
+    public abstract class MeasureDurations : IDraw
+    {
+        Dictionary<string, MeasureDurationItem> measurements = new();
+        int stack = 0;
+
+        public IDisposable Measure(string name)
+        {
+            MeasureDurationItem? m;
+            if (!measurements.TryGetValue(name, out m))
+            {
+                m = new MeasureDurationItem(stack);
+                measurements.Add(name, m);
+            }
+            m.MeasureBegin();
+
+            stack++;
+
+
+            return new OnDispose(() => { m.MeasureEnd(); stack--; });
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            string textNumbers = "";
+            string textNames = "";
+            foreach (var m in measurements)
+            {
+                textNumbers += m.Value.StackStr + m.Value.GetAverageMS().ToString("N1") + "\n";
+                textNames += m.Value.StackStr + "    - " + m.Key + "\n";
+            }
+            if (textNumbers.Length > 0)
+                textNumbers = textNumbers.Remove(textNumbers.Length - 1);
+            if (textNames.Length > 0)
+                textNames = textNames.Remove(textNames.Length - 1);
+
+            DrawSelf(spriteBatch, textNumbers, textNames);
+        }
+
+        protected abstract void DrawSelf(SpriteBatch spriteBatch, string textNumbers, string textNames);
+    }
+}
