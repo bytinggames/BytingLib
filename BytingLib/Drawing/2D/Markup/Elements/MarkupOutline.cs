48bcf7bba78c15fd0c3736e8cd21ddb7b4cd5206
Byting Games <bytinggames@gmail.com>
2023-03-29 17:28:39 +0200

    allow no color for MarkupOutline to use default text color

--- BytingLib/Drawing/2D/Markup/Elements/MarkupOutline.cs
+++ BytingLib/Drawing/2D/Markup/Elements/MarkupOutline.cs
@@ -3,18 +3,20 @@
     [MarkupShortcut("outline")]
     public class MarkupOutline : MarkupCollection
     {
-        Color color;
-        float thickness;
+        Color? color; // if null, takes color of text
+        float thickness = 1f;
         /// <summary>Negative for only bottom outline (used for drawing over the underline).</summary>
         int quality = 4;
 
         public bool SizeUnion { get; set; }
 
+        public MarkupOutline(Creator creator, string text)
+            : base(creator, text)
+        { }
         public MarkupOutline(Creator creator, string hexColor, string text)
             : base(creator, text)
         {
             color = ColorExtension.FromHex(hexColor);
-            thickness = 1f;
         }
         public MarkupOutline(Creator creator, string hexColor, float thickness, string text)
             : base(creator, text)
@@ -33,13 +35,13 @@
 
         public override string ToString()
         {
-            return $"Outline #{color.ToHex()} {base.ToString()}";
+            return $"Outline #{color?.ToHex()} {base.ToString()}";
         }
 
         public override IEnumerable<ILeaf> IterateOverLeaves(MarkupSettings settings)
         {
             var temp = settings.TextOutline?.CloneOutline();
-            settings.TextOutline = new MarkupSettings.Outline(color, thickness, SizeUnion, quality);
+            settings.TextOutline = new MarkupSettings.Outline(color ?? settings.TextColor, thickness, SizeUnion, quality);
 
             foreach (var leaf in base.IterateOverLeaves(settings))
                 yield return leaf;
