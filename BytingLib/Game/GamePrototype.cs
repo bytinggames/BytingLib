e5494d5ed428949f458de94c91cdd99414fea794
Byting Games <bytinggames@gmail.com>
2023-03-31 13:52:07 +0200

    +

--- BytingLib/Game/GamePrototype.cs
+++ BytingLib/Game/GamePrototype.cs
@@ -17,7 +17,7 @@ namespace BytingLib
         private Action? startRecordingPlayback;
 
         /// <summary>Only used for input that shouldn't be recorded (Fullscreen Toggle for example or Replay interrupt).
-        /// The difference to InputStuff.KeysDev is, that those can only be used in Dev mode</summary>
+        /// The difference to InputStuff.KeysDev</summary>
         protected KeyInput metaKeys;
 
         public GamePrototype(GameWrapper g, DefaultPaths paths, ContentConverter contentConverter,
