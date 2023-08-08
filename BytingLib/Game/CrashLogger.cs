27fd704d599e34fce2d03bec30311b99c2933e95
Byting Games <bytinggames@gmail.com>
2023-08-08 19:43:06 +0200

    added a new method to CrashLogger

--- BytingLib/Game/CrashLogger.cs
+++ BytingLib/Game/CrashLogger.cs
@@ -6,6 +6,30 @@ namespace BytingLib
 {
     public class CrashLogger
     {
+        public static void Catch(Exception e, string crashLogFilePath, string fontAssetName)
+        {
+            string message = DateTime.Now.ToString("dd.MM.yyy HH:mm:ss") + ": Game crashed!\n" + e;
+            AppendLog(crashLogFilePath, message);
+
+            try
+            {
+                string displayMessage = message + "\n\nlogged to file: " + crashLogFilePath;
+                Console.WriteLine(displayMessage); // this gets printed to linux terminal
+                using var messageBox = new MessageBox(message, fontAssetName);
+                messageBox.Run();
+            }
+            catch (Exception e2)
+            {
+                message = "MessageBox crashed:\n" + e2;
+                AppendLog(crashLogFilePath, message);
+            }
+        }
+        static void AppendLog(string logFile, string message)
+        {
+            File.AppendAllText(logFile, "\n" + message + "\n");
+        }
+
+
         public static void TryRun(string crashLogFilePath, string fontAssetName, Action run)
         {
             try
@@ -30,10 +54,6 @@ namespace BytingLib
                     AppendLog(crashLogFilePath, message);
                 }
             }
-        }
-        static void AppendLog(string logFile, string message)
-        {
-            File.AppendAllText(logFile, "\n" + message + "\n");
         }
     }
 }
