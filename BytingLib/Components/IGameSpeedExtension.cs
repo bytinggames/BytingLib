9ae41d89c157c03f75787d97d6efcc8977f47ca1
Byting Games <bytinggames@gmail.com>
2023-08-14 12:19:16 +0200

    IGameSpeedExtension+

--- BytingLib/Components/IGameSpeedExtension.cs
+++ BytingLib/Components/IGameSpeedExtension.cs
@@ -6,5 +6,18 @@
         {
             return gameSpeed.GameTime.TotalGameTime.TotalMilliseconds;
         }
+        public static double TotalSeconds(this IGameSpeed gameSpeed)
+        {
+            return gameSpeed.GameTime.TotalGameTime.TotalSeconds;
+        }
+
+        public static float TotalMSF(this IGameSpeed gameSpeed)
+        {
+            return (float)gameSpeed.GameTime.TotalGameTime.TotalMilliseconds;
+        }
+        public static float TotalSecondsF(this IGameSpeed gameSpeed)
+        {
+            return (float)gameSpeed.GameTime.TotalGameTime.TotalSeconds;
+        }
     }
 }
