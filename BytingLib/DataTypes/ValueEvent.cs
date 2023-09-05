e9adfbd7e0f79b84d01316de93292f6081323bfb
Byting Games <julian@bytinggames.com>
2023-09-05 09:05:10 +0200

    ValueEvent

---
+++ BytingLib/DataTypes/ValueEvent.cs
@@ -0,0 +1,28 @@
+﻿namespace BytingLib
+{
+    public class ValueEvent<T> where T : struct
+    {
+        private T _value;
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                if (!EqualityComparer<T>.Default.Equals(value, _value))
+                {
+                    _value = value;
+                    OnChange?.Invoke(value);
+                }
+            }
+        }
+
+        public ValueEvent() { }
+
+        public ValueEvent(T val)
+        {
+            _value = val;
+        }
+
+        public event Action<T>? OnChange;
+    }
+}
