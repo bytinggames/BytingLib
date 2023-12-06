58b5ec2b84458b0197cef1fb9d450edba84f203a
Julian <julian.hoeltge@gmail.com>
2023-12-06 20:24:43 +0100

    UpdateKeyPressed can now be updated when below popup

--- BytingLib/Input/UpdateKeyPressed.cs
+++ BytingLib/Input/UpdateKeyPressed.cs
@@ -3,17 +3,19 @@ using Microsoft.Xna.Framework.Input;
 
 namespace BytingLib
 {
-    public class UpdateKeyPressed : IUpdate
+    public class UpdateKeyPressed : IUpdate, IUpdateWhenBelowPopup
     {
         private readonly KeyInput keys;
         private readonly Keys key;
         private readonly Action action;
+        private readonly bool alsoUpdateBelowPopup;
 
-        public UpdateKeyPressed(KeyInput keys, Keys key, Action action)
+        public UpdateKeyPressed(KeyInput keys, Keys key, Action action, bool alsoUpdateBelowPopup = false)
         {
             this.keys = keys;
             this.key = key;
             this.action = action;
+            this.alsoUpdateBelowPopup = alsoUpdateBelowPopup;
         }
 
         public void Update()
@@ -21,6 +23,14 @@ namespace BytingLib
             if (keys.GetKey(key).Pressed)
             {
                 action();
+            }
+        }
+
+        public void UpdateWhenBelowPopup(Scene popup)
+        {
+            if (alsoUpdateBelowPopup)
+            {
+                Update();
             }
         }
     }
