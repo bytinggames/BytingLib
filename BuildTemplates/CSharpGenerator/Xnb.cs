ebb6598ed2600e6f03a157d1cb86a36e98a2e3c8
Byting Games <julian@bytinggames.com>
2024-03-19 16:22:58 +0100

    fixed the Use() method inside the generated ContentLoader.Generated.cs file

--- BuildTemplates/CSharpGenerator/Xnb.cs
+++ BuildTemplates/CSharpGenerator/Xnb.cs
@@ -4,7 +4,7 @@
     {
         public string AssetName { get; }
         public string FilePath { get; }
-        /// <summary>The same as asset name but without the parent folders</summary>
+        /// <summary>The same as asset name but without the extension</summary>
         public string FileName { get; set; }
         public string FileNameWithoutExtension => Path.GetFileNameWithoutExtension(FileName);
         public string CSharpDataType { get; }
@@ -14,13 +14,17 @@
 
         public Xnb(string filePath, string cSharpDataType, string varNameExtension)
         {
-            AssetName = FileName = FilePath = filePath;
+            AssetName = Path.GetFileName(filePath);
+
+            FileName = FilePath = filePath;
             CSharpDataType = cSharpDataType;
             VarNameExtension = varNameExtension;
 
             int dotIndex = AssetName.LastIndexOf('.');
             if (dotIndex != -1)
+            {
                 AssetName = AssetName.Remove(dotIndex);
+            }
         }
 
         public override string ToString()
@@ -31,9 +35,13 @@
         public string? PrintDeclare(bool loadOnStartup)
         {
             if (loadOnStartup)
+            {
                 return $"public Ref<{CSharpDataType}> {VarName} {{ get; }}";
+            }
             else
+            {
                 return $"public Ref<{CSharpDataType}> {GetInitCode(loadOnStartup)}";
+            }
         }
 
         public string? PrintInit(bool loadOnStartup)
@@ -43,10 +51,15 @@
                 return GetInitCode(loadOnStartup);
             }
             else
+            {
                 return null;
+            }
         }
 
-        private string GetInitCode(bool loadOnStartup) => $"{VarName} ={(loadOnStartup ? "" : ">")} disposables.Use(collector.Use<{CSharpDataType}>(\"{AssetName}\"));";
+        private string GetInitCode(bool loadOnStartup)
+        {
+            return $"{VarName} ={(loadOnStartup ? "" : ">")} disposables.Use(collector.Use<{CSharpDataType}>(basePath + \"{AssetName}\"));";
+        }
 
         internal static string ToVariableName(string name)
         {
