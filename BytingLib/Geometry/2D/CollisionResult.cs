b6999e7d7662aacf8bbf4f15c6265e20e7d3b38c
Byting Games <julian@bytinggames.com>
2024-08-27 20:52:47 +0200

    Grid2 update

--- BytingLib/Geometry/2D/CollisionResult.cs
+++ BytingLib/Geometry/2D/CollisionResult.cs
@@ -59,5 +59,39 @@ ColCornerIndex: {ColCornerIndex}";
             ColCornerPoly = cr.ColCornerPoly;
             ColCornerIndex = cr.ColCornerIndex;
         }
+
+        /// <summary>Applies the values of cr to this instance, that result in a larger collision shape.</summary>
+        public bool MinResult(CollisionResult cr)
+        {
+            if (cr.DistanceReversed.HasValue &&
+                (!DistanceReversed.HasValue || cr.DistanceReversed > DistanceReversed))
+            {
+                CopyBackwardValues(cr);
+            }
+
+            if (cr.Distance.HasValue &&
+                (!Distance.HasValue || cr.Distance < Distance))
+            {
+                CopyForwardValues(cr);
+
+                return true;
+            }
+            return false;
+        }
+
+
+        public bool MinResultIfCollisionInPresentOrFuture(CollisionResult cr)
+        {
+            if (cr.DistanceReversed.HasValue)
+            {
+                // is collision in positive distance?
+                // or is a collision happening right now?
+                if (cr.DistanceReversed >= 0)
+                {
+                    return MinResult(cr);
+                }
+            }
+            return false;
+        }
     }
 }
