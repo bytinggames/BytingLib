5ba4f2b5d97facb43667d8d9648cdb4207a103be
Byting Games <julian@bytinggames.com>
2024-09-24 11:33:51 +0200

    QoiProcessor premultiplies alpha now per default

--- BytingPipeline/Code/Qoi/QoiProcessor.cs
+++ BytingPipeline/Code/Qoi/QoiProcessor.cs
@@ -2,12 +2,16 @@
 using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
 using QoiSharp;
 using System;
+using System.ComponentModel;
 
 namespace BytingPipeline
 {
     [ContentProcessor(DisplayName = "QoiProcessor")]
     public class QoiProcessor : ContentProcessor<TextureContent, QoiTextureContent>
     {
+        [DefaultValue(true)]
+        public virtual bool PremultiplyAlpha { get; set; } = true;
+
         public override QoiTextureContent Process(TextureContent input, ContentProcessorContext context)
         {
             if (input.Faces.Count == 0
@@ -17,7 +21,23 @@ namespace BytingPipeline
             }
 
             var face = input.Faces[0];
-            QoiImage qoiImage = new QoiImage(face[0].GetPixelData(), face[0].Width, face[0].Height, QoiSharp.Codec.Channels.RgbWithAlpha);
+            var pixelData = face[0].GetPixelData();
+            if (PremultiplyAlpha)
+            {
+                for (int i = 0; i < pixelData.Length; i += 4)
+                {
+                    int a = pixelData[i + 3];
+                    if (a != 255)
+                    {
+                        // Premultiply Aplha (from Color.FromNonPremultiplied)
+                        pixelData[i] = (byte)(pixelData[i] * a / 255);
+                        pixelData[i + 1] = (byte)(pixelData[i + 1] * a / 255);
+                        pixelData[i + 2] = (byte)(pixelData[i + 2] * a / 255);
+                    }
+                }
+            }
+            QoiImage qoiImage = new QoiImage(pixelData, face[0].Width, face[0].Height, QoiSharp.Codec.Channels.RgbWithAlpha);
+
             byte[] qoi = QoiEncoder.Encode(qoiImage);
 
             return new QoiTextureContent(qoi);
