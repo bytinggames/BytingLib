fe0d91ec8743fca6e364d541d0ba044d833fb17d
Byting Games <julian@bytinggames.com>
2024-10-14 13:54:41 +0200

    JsonNode.TryGetValueFromString()

---
+++ BytingLib/StaticUtilities/Extensions/JsonNodeExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Nodes;
+
+namespace BytingLib
+{
+    public static class JsonNodeExtension
+    {
+        public static T? TryGetValueFromString<T>(this JsonNode node) where T : struct
+        {
+            return (T?)Convert.ChangeType(node.GetValue<string>(), typeof(T));
+        }
+    }
+}
