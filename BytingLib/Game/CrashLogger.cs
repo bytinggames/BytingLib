77e8c82961bf189b45330fb3a9f65a493a4c33b8
Byting Games <julian@bytinggames.com>
2024-11-28 09:42:02 +0100

    crash log in utc time

--- BytingLib/Game/CrashLogger.cs
+++ BytingLib/Game/CrashLogger.cs
@@ -8,7 +8,7 @@
         }
         public static void Catch(string message, string crashLogFilePath, string fontAssetName)
         {
-            message = DateTime.Now.ToString("dd.MM.yyy HH:mm:ss") + ": Game crashed.\n" + message;
+            message = DateTime.UtcNow.ToString("dd.MM.yyy HH:mm:ss") + " UTC: Game crashed.\n" + message;
             AppendLog(crashLogFilePath, message);
 
             try
@@ -38,7 +38,7 @@
             }
             catch (Exception e)
             {
-                string message = DateTime.Now.ToString("dd.MM.yyy HH:mm:ss") + ": Game crashed!\n" + e;
+                string message = DateTime.UtcNow.ToString("dd.MM.yyy HH:mm:ss") + " UTC: Game crashed!\n" + e;
                 AppendLog(crashLogFilePath, message);
 
                 try
