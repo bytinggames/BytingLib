448a236897d0ac5764778ad83fb514d420ab68ca
Byting Games <julian@bytinggames.com>
2024-12-03 15:54:35 +0100

    float extension+

--- BytingLib/StaticUtilities/Extensions/FloatExtension.cs
+++ BytingLib/StaticUtilities/Extensions/FloatExtension.cs
@@ -38,5 +38,31 @@
 
             return *(float*)&val;
         }
+        /// <summary>
+        /// Remap a float value in an input range proportional to a corresponding output range
+        /// </summary>
+        /// <param name="value">Input value</param>
+        /// <param name="rangeInMin">Input range lower bound</param>
+        /// <param name="rangeInMax">Input range upper bound</param>
+        /// <param name="rangeOutMin">Output range lower bound</param>
+        /// <param name="rangeOutMax">Output range upper bound</param>
+        public static float MapRange(this float value, float rangeInMin, float rangeInMax, float rangeOutMin, float rangeOutMax)
+        {
+            float mix = (value - rangeInMin) / (rangeInMax - rangeInMin);
+            return mix * (rangeOutMax - rangeOutMin) + rangeOutMin;
+        }
+
+        /// <summary>
+        /// Remap a float value in an input range to a corresponding output range. Clamp output to the bounds of the output range.
+        /// </summary>
+        /// <param name="value">Input value</param>
+        /// <param name="rangeInMin">Input range lower bound</param>
+        /// <param name="rangeInMax">Input range upper bound</param>
+        /// <param name="rangeOutMin">Output range lower bound</param>
+        /// <param name="rangeOutMax">Output range upper bound</param>
+        public static float MapRangeClamped(this float value, float rangeInMin, float rangeInMax, float rangeOutMin, float rangeOutMax)
+        {
+            return Math.Clamp(value.MapRange(rangeInMin, rangeInMax, rangeOutMin, rangeOutMax), rangeOutMin, rangeOutMax);
+        }
     }
 }
