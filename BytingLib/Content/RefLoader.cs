8d97a9291f1bfbfe14bf2c385e1d6c854c0a0e07
Byting Games <julian@bytinggames.com>
2025-01-24 22:30:39 +0100

    improved generated ContentLoader

---
+++ BytingLib/Content/RefLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace BytingLib
+{
+    public record RefLoaderDependencies(IContentCollector Collector, DisposableContainer Disposables)
+    {
+        public Ref<T> Use<T>(string assetPath)
+        {
+            return Disposables.Use(Collector.Use<T>(assetPath));
+        }
+        public void Override<T>(string assetPath, Ref<T> asset)
+        {
+            Collector.Override(assetPath, asset);
+        }
+    }
+
+    public class RefLoader<T>(RefLoaderDependencies d, string path)
+    {
+        private readonly RefLoaderDependencies d = d;
+        private readonly string path = path;
+
+        public Ref<T> Use() => d.Use<T>(path);
+        public void Override(Ref<T> asset) => d.Override(path, asset);
+    }
+}
