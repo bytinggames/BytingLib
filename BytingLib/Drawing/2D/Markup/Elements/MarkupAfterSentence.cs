f9359fcf9d8a3fb68bd507e3ab11bb492963c914
Byting Games <julian@bytinggames.com>
2025-01-26 10:28:03 +0100

    #after_sentence() for adding characters after each sentence

---
+++ BytingLib/Drawing/2D/Markup/Elements/MarkupAfterSentence.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BytingLib.Markup
+{
+    [MarkupShortcut("after_sentence")]
+    class MarkupAfterSentence : MarkupCollection
+    {
+        public MarkupAfterSentence(Creator creator, string text, string append)
+            : base(creator, AppendAfterSentence(text, append))
+        {
+        }
+
+        static string AppendAfterSentence(string text, string append)
+        {
+            return Regex.Replace(text, "[;.!?！。]", f => f + append);
+        }
+    }
+}
