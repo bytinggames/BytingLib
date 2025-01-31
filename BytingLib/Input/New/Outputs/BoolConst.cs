424c8fa6f2d2dfc84955e58d4a3e6a709b91055b
Byting Games <julian@bytinggames.com>
2025-01-31 15:38:43 +0100

    BoolConst

---
+++ BytingLib/Input/New/Outputs/BoolConst.cs
@@ -0,0 +1,22 @@
+﻿namespace BytingLib
+{
+    public class BoolConst : BoolInput
+    {
+        private readonly bool value;
+
+        public BoolConst(bool value)
+        {
+            this.value = value;
+        }
+
+        public override IEnumerable<InputUpdate> GetChildren()
+        {
+            yield break;
+        }
+
+        public override bool IsDown(FullInput input)
+        {
+            return value;
+        }
+    }
+}
