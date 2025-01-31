ef8a0c9633388007e712751ee2c076ab4dbb3f49
Byting Games <julian@bytinggames.com>
2025-01-31 17:42:24 +0100

    fixed mouse wheel

--- BytingLib/Input/New/Inputs/IntMouseWheel.cs
+++ BytingLib/Input/New/Inputs/IntMouseWheel.cs
@@ -2,6 +2,8 @@
 {
     public class IntMouseWheel : IntInput
     {
+        int previousScrollWheelValue;
+
         public override IEnumerable<InputUpdate> GetChildren()
         {
             yield break;
@@ -9,7 +11,17 @@
 
         public override int GetValue(FullInput input)
         {
-            return input.MouseState.ScrollWheelValue;
+            int scroll = input.MouseState.ScrollWheelValue;
+            long scrollDiff = (long)scroll - previousScrollWheelValue;
+            if (Math.Abs(scrollDiff) >= int.MaxValue) // check if value overflew (int.MaxValue is only half the range, but still suffices, because you can't scroll that much in a single update)
+            {
+                // correct overflow
+                const long intRange = (long)int.MaxValue * 2 + 1;
+                scrollDiff = Math.Sign(-scrollDiff) * (intRange - Math.Abs(scrollDiff));
+            }
+
+            previousScrollWheelValue = scroll;
+            return (int)scrollDiff;
         }
 
         public override string ToString()
