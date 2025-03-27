fcf1fcde3b8179bf33f92da9d46d1294ffdf8089
Byting Games <julian@bytinggames.com>
2025-03-27 16:55:26 +0100

    Vector4.XYZ()

---
+++ BytingLib/StaticUtilities/Extensions/Vector4Extension.cs
@@ -0,0 +1,10 @@
+﻿namespace BytingLib
+{
+    public static class Vector4Extension
+    {
+        public static Vector3 XYZ(this Vector4 v)
+        {
+            return new Vector3(v.X, v.Y, v.Z);
+        }
+    }
+}
