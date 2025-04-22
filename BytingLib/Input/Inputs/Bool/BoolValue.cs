dee07d152749ed987b2f9f4a495f92d46f471494
Byting Games <julian@bytinggames.com>
2025-04-22 11:34:01 +0200

    BoolValue, BoolValueFunc

---
+++ BytingLib/Input/Inputs/Bool/BoolValue.cs
@@ -0,0 +1,13 @@
+﻿namespace BytingLib
+{
+    public class BoolValue(bool value) : InputBoolSimple
+    {
+        public bool Value { get; set; } = value;
+
+        protected override bool CalculateValue(FullInput input, InputBoolState state)
+        {
+            return Value;
+        }
+    }
+}
+
---
+++ BytingLib/Input/Inputs/Bool/BoolValueFunc.cs
@@ -0,0 +1,13 @@
+﻿namespace BytingLib
+{
+    public class BoolValueFunc(Func<bool> getBool) : InputBoolSimple
+    {
+        private readonly Func<bool> getBool = getBool;
+
+        protected override bool CalculateValue(FullInput input, InputBoolState state)
+        {
+            return getBool();
+        }
+    }
+}
+
