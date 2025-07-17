307b73f1f7efdd62734ab0c760316a11f27a9650
Byting Games <julian@bytinggames.com>
2025-07-17 18:28:29 +0200

    fixed mouse wheel state when wheeling while not updating the input

--- BytingLib/Input/Inputs/Int/IntMouseWheel.cs
+++ BytingLib/Input/Inputs/Int/IntMouseWheel.cs
@@ -1,10 +1,23 @@
-﻿namespace BytingLib
+﻿using System.Diagnostics;
+
+namespace BytingLib
 {
     public class IntMouseWheel : InputInt<IntMouseWheelState>
     {
+        Stopwatch sw = new();
+
         protected override int CalculateValue(FullInput input, IntMouseWheelState state)
         {
-            return state.CalculateValue(input.MouseState.ScrollWheelValue);
+            long ms = sw.IsRunning ? sw.ElapsedMilliseconds : 0;
+
+            sw.Restart();
+            int diff = state.CalculateValue(input.MouseState.ScrollWheelValue);
+
+            if (ms > 100) // if 100ms passed since last update, we likely experienced no updates here
+            {
+                return 0;
+            }
+            return diff;
         }
 
         public override IntMouseWheelState CreateState(InputUpdater updater)
