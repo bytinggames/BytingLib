acb31b5804ea6f60eae8fa2653ea236d03fd7806
Byting Games <julian@bytinggames.com>
2025-12-17 21:55:55 +0100

    StringEditToUpperInvariant+

--- BytingLib/UI/StringEdit/StringEditToUpperInvariant.cs
+++ BytingLib/UI/StringEdit/StringEditToUpperInvariant.cs
@@ -2,9 +2,35 @@
 {
     public class StringEditToUpperInvariant : IStringEdit
     {
+        private readonly Predicate<char>? when;
+
+        public StringEditToUpperInvariant(Predicate<char>? when = null)
+        {
+            this.when = when;
+        }
+
         public string GetApplied(string str)
         {
-            return str.ToUpperInvariant();
+            if (when == null)
+            {
+                return str.ToUpperInvariant();
+            }
+            else
+            {
+                string strOutput = "";
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (when(str[i]))
+                    {
+                        strOutput += char.ToUpperInvariant(str[i]);
+                    }
+                    else
+                    {
+                        strOutput += str[i];
+                    }
+                }
+                return strOutput;
+            }
         }
     }
 }
