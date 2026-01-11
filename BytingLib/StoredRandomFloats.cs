85685e81bde16abb16eb022a1a0270f3663accf9
Byting Games <julian@bytinggames.com>
2026-01-11 11:30:46 +0100

    StoredRandomFloats

---
+++ BytingLib/StoredRandomFloats.cs
@@ -0,0 +1,40 @@
+﻿namespace BytingLib
+{
+    public class StoredRandomFloats
+    {
+        private readonly Random rand;
+        private List<float> floats = new();
+        private int index;
+
+        public StoredRandomFloats(Random rand)
+        {
+            this.rand = rand;
+        }
+
+        public void ClearValues()
+        {
+            index = 0;
+            floats.Clear();
+        }
+
+        public float Get(Func<Random, float> getNext)
+        {
+            if (index >= floats.Count)
+            {
+                float val = getNext(rand);
+                floats.Add(val);
+                index = floats.Count + 1;
+                return val;
+            }
+            else
+            {
+                return floats[index++];
+            }
+        }
+
+        public void Begin()
+        {
+            index = 0;
+        }
+    }
+}
