43747eba06617cb4391c46cdb3053dac72db1172
Byting Games <julian@bytinggames.com>
2026-01-18 15:50:21 +0100

    #space MarkupSpace

---
+++ BytingLib/Drawing/2D/Markup/Elements/MarkupSpace.cs
@@ -0,0 +1,33 @@
+﻿namespace BytingLib.Markup
+{
+    [MarkupShortcut("space")]
+    public class MarkupSpace : ILeaf
+    {
+        public bool ConfinesToLineSpacing => false;
+
+        public float Width { get; }
+        public float Height { get; }
+
+        public MarkupSpace(float width, float height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public void Draw(MarkupSettings settings) { }
+
+        public Vector2 GetSize(MarkupSettings settings, int start, int end)
+        {
+            return new Vector2(Width, Height);
+        }
+
+        public override string ToString()
+        {
+            return "\\n";
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
