840c610cad21fbdb5a2dbec99e265064a45a2809
Byting Games <julian@bytinggames.com>
2026-03-22 15:32:39 +0100

    Canvas.Navigate(Element)

--- BytingLib/UI/Canvas/Canvas.cs
+++ BytingLib/UI/Canvas/Canvas.cs
@@ -178,35 +178,7 @@
 
             if (navigateTo != null)
             {
-                NavigateElement = navigateTo;
-
-                if (NavigateElement is TextInput textInput2)
-                {
-                    Input.FocusElement = textInput2;
-                }
-
-                var scroll = navigateTo.GetParents().OfType<PanelScroll>().FirstOrDefault();
-                if (scroll != null)
-                {
-                    if (!navigateTo.AbsoluteRect.IsEnclosedIn(scroll.AbsoluteRect))
-                    {
-                        float scrollBy = 0f;
-                        if (navigateTo.AbsoluteRect.Top < scroll.AbsoluteRect.Top)
-                        {
-                            scrollBy = navigateTo.AbsoluteRect.Top - scroll.AbsoluteRect.Top;
-                        }
-                        else if (navigateTo.AbsoluteRect.Bottom > scroll.AbsoluteRect.Bottom)
-                        {
-                            scrollBy = navigateTo.AbsoluteRect.Bottom - scroll.AbsoluteRect.Bottom;
-                        }
-                        if (scrollBy != 0f)
-                        {
-                            scroll.Scroll += scrollBy;
-                        }
-                    }
-                }
-
-                OnNavigate?.Invoke(NavigateElement);
+                Navigate(navigateTo);
             }
         }
 
@@ -399,6 +371,39 @@
             }
 
             return bestScoreElement;
+        }
+
+        public void Navigate(Element navigateTo)
+        {
+            NavigateElement = navigateTo;
+
+            if (NavigateElement is TextInput textInput2)
+            {
+                Input.FocusElement = textInput2;
+            }
+
+            var scroll = navigateTo.GetParents().OfType<PanelScroll>().FirstOrDefault();
+            if (scroll != null)
+            {
+                if (!navigateTo.AbsoluteRect.IsEnclosedIn(scroll.AbsoluteRect))
+                {
+                    float scrollBy = 0f;
+                    if (navigateTo.AbsoluteRect.Top < scroll.AbsoluteRect.Top)
+                    {
+                        scrollBy = navigateTo.AbsoluteRect.Top - scroll.AbsoluteRect.Top;
+                    }
+                    else if (navigateTo.AbsoluteRect.Bottom > scroll.AbsoluteRect.Bottom)
+                    {
+                        scrollBy = navigateTo.AbsoluteRect.Bottom - scroll.AbsoluteRect.Bottom;
+                    }
+                    if (scrollBy != 0f)
+                    {
+                        scroll.Scroll += scrollBy;
+                    }
+                }
+            }
+
+            OnNavigate?.Invoke(NavigateElement);
         }
 
         public override void Update(ElementInput input)
