42b743a95551c8b671b4efb95c1ca8b2cc83215d
Byting Games <julian@bytinggames.com>
2026-04-10 13:10:36 +0200

    increased performance of StoredRandomFloats

--- BytingLib/StoredRandomFloats.cs
+++ BytingLib/StoredRandomFloats.cs
@@ -17,19 +17,35 @@
             floats.Clear();
         }
 
-        public float Get(Func<Random, float> getNext)
+        public float NextSingle(float min, float max)
         {
             if (index >= floats.Count)
             {
-                float val = getNext(rand);
-                floats.Add(val);
-                index = floats.Count + 1;
-                return val;
+                return Add(rand.NextSingle(min, max));
             }
             else
             {
                 return floats[index++];
             }
+        }
+
+        public float NextSinglePow(float min, float max, float power)
+        {
+            if (index >= floats.Count)
+            {
+                return Add(rand.NextSinglePow(min, max, power));
+            }
+            else
+            {
+                return floats[index++];
+            }
+        }
+
+        private float Add(float val)
+        {
+            floats.Add(val);
+            index = floats.Count + 1;
+            return val;
         }
 
         public void Begin()
